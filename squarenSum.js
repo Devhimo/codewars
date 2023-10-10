3ece506eafc680b9bfa9c1d9e14e1df23fb8335d
Faheem Jackson <faheemrhodajackson@gmail.com>
2023-10-10 07:57:19 +0200

    added new coding challenge Square numbers

---
+++ squarenSum.js
@@ -0,0 +1,30 @@
+// DESCRIPTION:
+// Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+// For example, for [1, 2, 2] it should return 9 because 
+// 1
+// 2
+// +
+// 2
+// 2
+// +
+// 2
+// 2
+// =
+// 9
+// 1 
+// 2
+//  +2 
+// 2
+//  +2 
+// 2
+//  =9.
+
+// my solution 
+function squareSum(numbers){
+
+    let square = numbers.reduce((acc, c) => acc + c ** 2, 0);
+  
+    return square;
+  
+  }
